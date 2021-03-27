69bdc147078b590b67f119ca62e642a710cb26bd
BernarBerdikul <berdikul@appetite.menu>
2021-03-27 16:36:09 +0600

    prepare deploy

--- virtual_day/wsgi.py
+++ virtual_day/wsgi.py
@@ -1,5 +1,8 @@
 import os
 from configurations.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_day.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
