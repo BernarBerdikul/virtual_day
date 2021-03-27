ecb9813779aa9bc6a3182c5d5558addbbfdf84f6
BernarBerdikul <berdikul@appetite.menu>
2021-03-27 18:00:49 +0600

    prepare deploy

--- virtual_day/asgi.py
+++ virtual_day/asgi.py
@@ -5,9 +5,6 @@ from django.core.asgi import get_asgi_application
 from virtual_day.chat import routing
 from virtual_day.chat.middlewares import TokenAuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from dotenv import load_dotenv
-
-load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_day.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
--- virtual_day/wsgi.py
+++ virtual_day/wsgi.py
@@ -1,18 +1,7 @@
 import os
-import sys
 from configurations.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
-load_dotenv()
-
-path = '/root/project/virtual_day/'
-if path not in sys.path:
-    sys.path.append(path)
-
-print(os.getenv('DJANGO_SETTINGS_MODULE'))
-print(os.getenv('DJANGO_CONFIGURATION'))
-
-os.environ.setdefault(os.getenv('DJANGO_SETTINGS_MODULE'), 'virtual_day.settings')
-os.environ.setdefault(os.getenv('DJANGO_CONFIGURATION'), 'Dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_day.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 application = get_wsgi_application()
