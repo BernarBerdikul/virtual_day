ee4ae5d9bfa55a2b97d43057f5fc7db9fa0266e7
BernarBerdikul <berdikul@appetite.menu>
2021-03-27 16:03:45 +0600

    prepare deploy

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 command = '/root/project/env/bin/gunicorn'
 pythonpath = '/root/project/virtual_day'
-bind = '78.140.223.130:8000'
+bind = '127.0.0.1:8001'
 workers = 3
 user = 'root'
 limit_request_fields = 32000
