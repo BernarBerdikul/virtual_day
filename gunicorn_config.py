e0e9d9fff81f25659666b09819505c66258b4643
BernarBerdikul <berdikul@appetite.menu>
2021-03-27 18:55:58 +0600

    prepare deploy

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,25 +5,24 @@ workers = 3
 user = 'root'
 limit_request_fields = 32000
 limit_request_fields_size = 0
-raw_env = [
-    "DJANGO_SETTINGS_MODULE=virtual_day.settings",
-    "DJANGO_CONFIGURATION=BaseConfiguration",
-    'SECRET_KEY="hw^7polec^hp18%%stj8k=$v#gsl#zu$%^%bgg908cswiph^w%"',
+raw_env = ["DJANGO_SETTINGS_MODULE=virtual_day.settings",
+           "DJANGO_CONFIGURATION=BaseConfiguration",
+           'SECRET_KEY="hw^7polec^hp18%%stj8k=$v#gsl#zu$%^%bgg908cswiph^w%"',
 
-    "DB_NAME=virtual_day",
-    "DB_USER=virtual_day",
-    "DB_PASSWORD=virtual_day",
-    "DB_HOST=localhost",
-    "DB_PORT=5432",
+           "DB_NAME=virtual_day",
+           "DB_USER=virtual_day",
+           "DB_PASSWORD=virtual_day",
+           "DB_HOST=localhost",
+           "DB_PORT=5432",
 
-    "EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'",
-    "EMAIL_HOST=smtp.gmail.com",
-    "EMAIL_HOST_USER='DaBEERman322@gmail.com'",
-    "EMAIL_HOST_PASSWORD=Aisultan12",
-    'FROM_EMAIL="default from emall"',
-    "EMAIL_PORT=25",
+           "EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'",
+           "EMAIL_HOST=smtp.gmail.com",
+           "EMAIL_HOST_USER='DaBEERman322@gmail.com'",
+           "EMAIL_HOST_PASSWORD=Aisultan12",
+           'FROM_EMAIL="default from emall"',
+           "EMAIL_PORT=25",
 
-    "LOGS_BASE_DIR=logs",
+           "LOGS_BASE_DIR=logs",
 
-    "GOOGLE_APPLICATION_CREDENTIALS='/root/project/virtual_day/firebase/ServiceAccount.json'"
-]
+           "GOOGLE_APPLICATION_CREDENTIALS='/root/project/virtual_day/firebase/ServiceAccount.json'"
+           ]
