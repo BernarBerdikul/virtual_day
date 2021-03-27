9af943e94ba00e229d950c4bb307ac051ed45f88
BernarBerdikul <berdikul@appetite.menu>
2021-03-27 20:13:43 +0600

    prepare deploy

--- virtual_day/settings.py
+++ virtual_day/settings.py
@@ -306,8 +306,8 @@ class Dev(BaseConfiguration):
     IS_LOCAL = False
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', "37.18.30.203"]
     SITE_URL = "http://37.18.30.203:8000"
-    STATIC_ROOT = '/root/project//virtual_day/static'
-    MEDIA_ROOT = '/root/project//virtual_day/media'
+    STATIC_ROOT = '/root/project/virtual_day/static'
+    MEDIA_ROOT = '/root/project/virtual_day/media'
     SECURE_SSL_REDIRECT = False
 
     CHANNEL_LAYERS = {
@@ -326,8 +326,8 @@ class Prod(BaseConfiguration):
     IS_TEST = False
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', "37.18.30.203"]
     SITE_URL = "http://37.18.30.203:8000"
-    STATIC_ROOT = '/root/project//virtual_day/static'
-    MEDIA_ROOT = '/root/project//virtual_day/media'
+    STATIC_ROOT = '/root/project/virtual_day/static'
+    MEDIA_ROOT = '/root/project/virtual_day/media'
     # SECURE_SSL_REDIRECT = True
 
     CHANNEL_LAYERS = {
