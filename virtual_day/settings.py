119d7bb9c6d35050f864f342676776309940157e
BernarBerdikul <berdikul@appetite.menu>
2021-03-28 16:09:05 +0600

    Change email send

--- virtual_day/settings.py
+++ virtual_day/settings.py
@@ -284,20 +284,13 @@ class BaseConfiguration(Configuration):
     #     },
     # }
 
-    # EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-    # EMAIL_HOST = os.getenv('EMAIL_HOST')
-    # EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-    # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    # FROM_EMAIL = os.getenv('FROM_EMAIL')
-    # EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'mail.nashe.menu'
-    EMAIL_HOST_USER = 'info@nashe.menu'
+    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-    EMAIL_HOST_PASSWORD = 'PZUYTFlXklanDMyS5mLr'
-    FROM_EMAIL = "default from emall"
-    EMAIL_PORT = 25
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    FROM_EMAIL = os.getenv('FROM_EMAIL')
+    EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
     EMAIL_USE_TLS = True
     EMAIL_ACTIVATION_SEND = False
 
