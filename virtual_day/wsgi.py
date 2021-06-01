78bcc68d716b23a6db2e63d457a176c45c22ea96
BernarBerdikul <berdikul@appetite.menu>
2021-06-02 01:57:11 +0600

    change exceptions

--- virtual_day/wsgi.py
+++ virtual_day/wsgi.py
@@ -1,7 +1,8 @@
 import os
-from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_day.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'BaseConfiguration')
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
