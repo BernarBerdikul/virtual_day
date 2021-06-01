d8e55e6f7f6e821960775e5f005705531fac4965
BernarBerdikul <berdikul@appetite.menu>
2021-06-02 02:05:48 +0600

    change wsgi

--- manage.py
+++ manage.py
@@ -6,9 +6,14 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_day.settings')
     os.environ.setdefault('DJANGO_CONFIGURATION', 'BaseConfiguration')
-
-    from configurations.management import execute_from_command_line
-
+    try:
+        from configurations.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
